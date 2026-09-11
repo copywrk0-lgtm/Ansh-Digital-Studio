03ffdd3265152247fd7fcdaf3e63b2b499e89ea9
copywrk0-lgtm <copywrk0@gmail.com>
2026-09-11 12:25:49 +0530

    Optimize portfolio image delivery

--- components/sections/GalleryGrid.tsx
+++ components/sections/GalleryGrid.tsx
@@ -16,7 +16,7 @@ export function GalleryGrid(){
   const selected=[portfolio[0],portfolio[1],portfolio[2],portfolio[3],portfolio[4],portfolio[5],portfolio[6],portfolio[7]];
   return <section className='galleryGrid section' aria-label='Selected wedding photography'>
     {selected.map((p,i)=><figure className={`galleryItem ${galleryLayout[i]}`} key={p.n}>
-      <div className='galleryMedia'><Image src={p.image} alt={`${p.type}: ${p.title}`} fill className='cover' quality={90} sizes='(max-width:800px) 100vw, 50vw'/></div>
+      <div className='galleryMedia'><Image src={p.image} alt={`${p.type}: ${p.title}`} fill className='cover' quality={84} sizes='(max-width:800px) 100vw, 50vw'/></div>
       <figcaption>
         <span className='galleryNumber'>{String(i+1).padStart(2,'0')}</span>
         <div><strong>{p.title}</strong><small>{p.type}</small></div>
