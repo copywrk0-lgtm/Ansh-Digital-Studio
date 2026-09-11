2ccff3f7737be21f5d1d8cd280baddf0be7608c3
copywrk0-lgtm <copywrk0@gmail.com>
2026-09-11 15:02:01 +0530

    Remove old studio story photo

--- components/sections/StorySection.tsx
+++ components/sections/StorySection.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import {Eyebrow} from '@/components/ui/Eyebrow';
 import {TextLink} from '@/components/ui/TextLink';
 
 export function StorySection(){return <section className='story section'>
   <div className='storyVisual'>
-    <div className='storyImage'><Image src='/images/bride-table.jpg' alt='Bride portrait in warm ambient light by Ansh Digital Studio' fill className='cover' sizes='(max-width:800px) 100vw, 42vw'/></div>
+    <div className='storyImage photoPlaceholder' aria-label='Photography placeholder'><div className='photoPlaceholderMark'>ANSH<small>studio portrait incoming</small></div></div>
     <p className='storyCaption'>Portraits that leave room for<br/>the person inside the picture.</p>
   </div>
   <div className='storyCopy'>
